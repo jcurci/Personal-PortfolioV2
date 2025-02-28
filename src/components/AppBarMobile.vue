e55a6950786e9146eed4466414f777698e086418
Jhonatan Curci <curcijho@gmail.com>
2025-02-28 12:35:07 -0300

    correção de menu lateral

--- src/components/AppBarMobile.vue
+++ src/components/AppBarMobile.vue
@@ -1,86 +1,89 @@
 <template>
-    <v-app-bar app color="#252525" elevation="24">
-      <v-toolbar-title>
-        <a href="/" class="logo-text" aria-label="Ir para a página inicial">
-          Curci
-          <v-icon class="mr-2 neon-icon" size="45" aria-hidden="true">
-            mdi-dev-to
-          </v-icon>
-        </a>
-      </v-toolbar-title>
-  
-      <v-spacer></v-spacer>
-  
-      <!-- Botão para abrir o menu hambúrguer -->
-      <v-btn icon @click="toggleDrawer" aria-label="Abrir menu">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-  
-      <!-- Menu lateral -->
-      <v-navigation-drawer v-model="drawer" app temporary color="#252525">
-        <v-list dense>
-          <v-list-item to="/" class="nav-button" aria-label="Ir para a home">
-            <v-list-item-icon><v-icon>mdi-home</v-icon></v-list-item-icon>
-            <v-list-item-content>Home</v-list-item-content>
-          </v-list-item>
-          <v-list-item to="/resumo" class="nav-button" aria-label="Ir para o resumo">
-            <v-list-item-icon><v-icon>mdi-file-account</v-icon></v-list-item-icon>
-            <v-list-item-content>Resume</v-list-item-content>
-          </v-list-item>
-          <v-list-item to="/Projetos" class="nav-button" aria-label="Ir para os projetos">
-            <v-list-item-icon><v-icon>mdi-folder-network-outline</v-icon></v-list-item-icon>
-            <v-list-item-content>Projects</v-list-item-content>
-          </v-list-item>
-          <v-list-item to="/contato" class="nav-button" aria-label="Ir para o contato">
-            <v-list-item-icon><v-icon>mdi-contacts</v-icon></v-list-item-icon>
-            <v-list-item-content>Contact</v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-    </v-app-bar>
-  </template>
-  
-  <script>
-  export default {
-    name: 'AppbarMobile',
-    data() {
-      return {
-        drawer: false, // Controle do menu
-      };
-    },
-    methods: {
-      toggleDrawer() {
-        this.drawer = !this.drawer;
-      }
-    }
-  };
-  </script>
-  
-  <style scoped>
-  @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap');
-  
-  a {
-    text-decoration: none;
-  }
-  
-  .logo-text {
-    color: #FFEA00;
-    font-family: 'Merriweather', serif;
-    font-size: 32px;
-    font-weight: 700;
-    letter-spacing: 1px;
-  }
-  
-  .nav-button {
-    color: #f7f7f7;
-    border-bottom: 1px solid #FFEA00;
-    background-color: transparent;
-    padding: 10px;
-  }
-  
-  .nav-button:hover {
-    background-color: #312d018e;
-    box-shadow: 0 2px 15px #FFEA00;
-  }
-  </style>
-  
+  <v-app-bar app floating color="#252525" elevation="24">
+    <v-toolbar-title>
+      <a href="/" class="logo-text">
+        Curci
+        <v-icon class="mr-2 neon-icon" size="45" aria-hidden="true">
+          mdi-dev-to
+        </v-icon>
+      </a>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon @click="toggleDrawer">
+      <v-icon>mdi-menu</v-icon>
+    </v-btn>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" app temporary location="right" color="#252525">
+
+    <v-list dense>
+      <v-list-item to="/" class="nav-button">
+        <v-icon>mdi-home</v-icon>
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item to="/resumo" class="nav-button">
+        <v-icon>mdi-file-account</v-icon>
+        <v-list-item-title>Resume</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item to="/Projetos" class="nav-button">
+        <v-icon>mdi-folder-network-outline</v-icon>
+        <v-list-item-title>Projects</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item to="/contato" class="nav-button">
+        <v-icon>mdi-contacts</v-icon>
+        <v-list-item-title>Contact</v-list-item-title>
+      </v-list-item>
+
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'AppbarMobile',
+  setup() {
+    const drawer = ref(false);
+
+    const toggleDrawer = () => {
+      drawer.value = !drawer.value;
+    };
+
+    return { drawer, toggleDrawer };
+  },
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap');
+
+a {
+  text-decoration: none;
+}
+
+.logo-text {
+  color: #FFEA00;
+  font-family: 'Merriweather', serif;
+  font-size: 32px;
+  font-weight: 700;
+  letter-spacing: 1px;
+}
+
+.nav-button {
+  color: #f7f7f7;
+  border-bottom: 1px solid #FFEA00;
+  background-color: transparent;
+  padding: 10px;
+}
+
+.nav-button:hover {
+  background-color: #312d018e;
+  box-shadow: 0 2px 15px #FFEA00;
+}
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app>
-    <!-- Exibir a versão correta da AppBar -->
     <AppbarMobile v-if="isMobile" />
     <Appbar v-else />
     
@@ -13,34 +12,31 @@
 </template>
 
 <script>
-import Appbar from '@/components/Appbar.vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 import AppbarMobile from '@/components/AppBarMobile.vue';
-import ScrollTop from '@/components/ScrollTop.vue';
-import AppFooter from '@/components/AppFooter.vue';
+import Appbar from '@/components/AppBar.vue';
 
 export default {
   components: {
-    Appbar,
     AppbarMobile,
-    ScrollTop,
-    AppFooter
+    Appbar
   },
-  data() {
-    return {
-      isMobile: false
+  setup() {
+    const isMobile = ref(window.innerWidth <= 768);
+
+    const checkScreenSize = () => {
+      isMobile.value = window.innerWidth <= 768;
     };
-  },
-  mounted() {
-    this.checkScreenSize();
-    window.addEventListener('resize', this.checkScreenSize);
-  },
-  beforeDestroy() {
-    window.removeEventListener('resize', this.checkScreenSize);
-  },
-  methods: {
-    checkScreenSize() {
-      this.isMobile = window.innerWidth <= 600; // Define o breakpoint para o mobile
-    }
+
+    onMounted(() => {
+      window.addEventListener('resize', checkScreenSize);
+    });
+
+    onBeforeUnmount(() => {
+      window.removeEventListener('resize', checkScreenSize);
+    });
+
+    return { isMobile };
   }
 };
 </script>
